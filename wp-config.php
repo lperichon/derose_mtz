31b6d3aba6ff2c1e45919202486d3d0478ed3e8d
Luis Perichon <luisperichon@gmail.com>
2012-11-21 12:25:26 -0300

    charset & collate are the same on every env

--- wp-config.php
+++ wp-config.php
@@ -35,13 +35,13 @@ else {
 
  /** MySQL hostname */
  define('DB_HOST', 'localhost');
-
- /** Database Charset to use in creating database tables. */
- define('DB_CHARSET', 'utf8');
-
- /** The Database Collate type. Don't change this if in doubt. */
- define('DB_COLLATE', '');
 }
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
